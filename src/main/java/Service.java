130338cf054023681e56f678eb1b9dbc5bf4cf5e
lewis.jones <lewis1997@live.com>
2025-01-21 17:20:57 +0000

    feat: add logging functionality

--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,6 +1,11 @@
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Service {
 
   public String helloWorld() {
+    log.debug("Calling helloWorld()...");
+    
     return "Hello, World!";
   }
 
