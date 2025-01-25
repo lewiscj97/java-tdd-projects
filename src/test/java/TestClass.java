a4939fe503c863506f03f01a864cfe5ae629da50
lewis.jones <lewis1997@live.com>
2025-01-25 18:55:47 +0000

    test: use parameterized tests for first two days

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,5 +1,6 @@
 import model.*;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import service.VideoStore;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,12 +9,12 @@ public class TestClass {
 
   VideoStore videoStore = new VideoStore();
 
-  @Test
-  public void generateStatementForUser_OneRegularMovie_OneDay() {
+  @ParameterizedTest
+  @ValueSource(ints = {1, 2})
+  public void generateStatementForUser_OneRegularMovie_2Pound_FirstTwoDays(int numberOfDays) {
     String name = "Tester McGee";
     User user = new User(name);
     Movie crazyNotes = new RegularMovie("Crazynotes");
-    Integer numberOfDays = 1;
     Rental rental = new Rental(crazyNotes, numberOfDays);
 
     Statement statement = videoStore.getStatement(user, rental);
