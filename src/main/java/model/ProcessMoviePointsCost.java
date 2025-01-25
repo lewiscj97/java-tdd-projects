9cadd248c890ce739a6cd9534741a23a349243bd
lewis.jones <lewis1997@live.com>
2025-01-25 19:21:08 +0000

    refactor: extract functionality remove duplication

---
+++ src/main/java/model/ProcessMoviePointsCost.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProcessMoviePointsCost {
+
+  int points;
+  double cost;
+}
--- src/main/java/service/VideoStore.java
+++ src/main/java/service/VideoStore.java
@@ -16,27 +16,38 @@ public class VideoStore {
     for (Rental rental : rentals) {
       Class<? extends Movie> movieType = rental.getMovie().getClass();
       int numberOfDays = rental.getNumberOfDays();
+      ProcessMoviePointsCost moviePointsCost;
+
       if (movieType.equals(RegularMovie.class)) {
-        rentalPoints++;
-        if (numberOfDays <= 2) {
-          totalCost += 2;
-        } else {
-          totalCost = 2 + (numberOfDays - 2) * 1.5;
-        }
+        moviePointsCost = processRegularMovies(numberOfDays);
       } else if (movieType.equals(NewReleaseMovie.class)) {
-        if (numberOfDays == 1) {
-          rentalPoints++;
-        } else {
-          rentalPoints += 2;
-        }
-        totalCost += numberOfDays * 3;
+        moviePointsCost = processNewMovies(numberOfDays);
+      } else {
+        moviePointsCost = new ProcessMoviePointsCost();
       }
+
+      totalCost += moviePointsCost.getCost();
+      rentalPoints += moviePointsCost.getPoints();
     }
 
     statement.setTotalCost(totalCost);
     statement.setRentalPoints(rentalPoints);
 
     return statement;
+  }
+
+  private ProcessMoviePointsCost processRegularMovies(int numberOfDays) {
+    int points = 1;
+    double cost = (numberOfDays <= 2) ? 2 : 2 + (numberOfDays - 2) * 1.5;
+
+    return new ProcessMoviePointsCost(points, cost);
+  }
+
+  private ProcessMoviePointsCost processNewMovies(int numberOfDays) {
+    double cost = numberOfDays * 3;
+    int points = (numberOfDays == 1) ? 1 : 2;
+
+    return new ProcessMoviePointsCost(points, cost);
   }
 
 }
