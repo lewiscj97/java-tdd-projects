c590a68a228c64ca7bd03d28a4533bf8097e5e39
lewis.jones <lewis1997@live.com>
2025-01-28 20:30:43 +0000

    refactor: before each test setup

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,17 +1,24 @@
 import model.Player;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestClass {
 
+  Player player1;
+  Player player2;
+  TennisGame game;
+
+  @BeforeEach
+  public void setup() {
+    player1 = new Player("player 1");
+    player2 = new Player("player 2");
+    game = new TennisGame(player1, player2);
+  }
+
   @Test
   public void setupOfTennisGame() {
-    Player player1 = new Player("player 1");
-    Player player2 = new Player("player 2");
-
-    TennisGame game = new TennisGame(player1, player2);
-
     assertEquals("player 1", game.getPlayer1().getName());
     assertEquals(0, game.getPlayer1().getScore());
     assertEquals("player 2", game.getPlayer2().getName());
