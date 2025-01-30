b2bd7491ab20cbe7986ab073de582163414ad987
lewis.jones <lewis1997@live.com>
2025-01-30 18:02:34 +0000

    test: add initial test for splitting string into list

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,18 +1,23 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestClass {
 
-  Service service = new Service();
+  StringSorter stringSorter = new StringSorter();
+  String TEST_INPUT = "Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi";
 
   @Test
-  public void initialTest() {
-    String expected = "Hello, World!";
+  public void splitInputIntoArray() {
+    String input = "Tadashi Takahiro Takao";
 
-    String output = service.helloWorld();
+    List<String> output = stringSorter.split(input);
 
-    assertEquals(expected, output);
+    assertEquals("Tadashi", output.get(0));
+    assertEquals("Takahiro", output.get(1));
+    assertEquals("Takao", output.get(2));
   }
 
 }
