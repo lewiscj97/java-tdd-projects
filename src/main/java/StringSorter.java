b12388266e45272630260aa623a0539fd58c21fd
lewis.jones <lewis1997@live.com>
2025-01-30 18:17:59 +0000

    feat: add functionality for sorting by longest string first

--- src/main/java/StringSorter.java
+++ src/main/java/StringSorter.java
@@ -1,12 +1,33 @@
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Slf4j
 public class StringSorter {
 
-  public List<String> split(String input) {
-    return List.of(input.split(" "));
+  public List<String> sort(String input) {
+    List<String> splitInput = new ArrayList<>(List.of(input.split(" ")));
+    List<String> output = new ArrayList<>();
+
+    int length = splitInput.size();
+
+    while (length != 0) {
+      String longestString = "";
+
+      for (String s : splitInput) {
+        if (s.length() > longestString.length()) {
+          longestString = s;
+        }
+      }
+
+      output.add(longestString);
+      splitInput.remove(longestString);
+      length--;
+    }
+
+    return output;
   }
 
 }
